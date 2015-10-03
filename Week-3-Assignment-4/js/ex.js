0c2258724f2a8f00517856d74ccd3745838b1280
lant0100 <lant0100@algonquinlive.com>
2015-10-02 21:30:37 -0400

    added ex.js

--- Week-3-Assignment-4/js/ex.js
+++ Week-3-Assignment-4/js/ex.js
@@ -1,3 +1,20 @@
-// JavaScript Document
+var Long = document.querySelector('.long-rect');
+var Short = document.querySelector('.short-rect');
+var Stopper = false;
 
+document.addEventListener('DOMContentLoaded', function onLoad() {
+  Long.addEventListener("mouseover", function one() {
+    Short.innerHTML = "Well done mouseover worked! Now take the mouse off this box and go click the right box!!!";
+  });
 
+  Long.addEventListener("mouseout", function two() {
+    Short.innerHTML = "You've moused out! Almost done go click this box now!!!";
+    Stopper = true;
+  });
+
+  Short.addEventListener("click", function three() {
+    if (Stopper) {
+      alert("Well done! Now submit!!");
+    }
+  });
+});
